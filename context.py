ca55e156b6658131693a77e60489b128faf639ce
primaeval <primaeval@users.noreply.github.com>
2016-11-05 10:19:07 +0100

    init

---
+++ context.py
@@ -0,0 +1,10 @@
+import xbmc
+import sys
+import urllib
+
+title = xbmc.getInfoLabel('ListItem.Label')
+path = xbmc.getInfoLabel('ListItem.FileNameAndPath')
+icon = xbmc.getInfoLabel('ListItem.Icon ')
+
+url = "plugin://plugin.program.simple.favourites/add_url/none/%s/%s/%s" % (urllib.quote_plus(title),urllib.quote_plus(path),urllib.quote_plus(icon))
+xbmc.executebuiltin("PlayMedia(%s)" % url)
